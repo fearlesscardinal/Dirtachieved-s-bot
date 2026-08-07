3eaff56a77b448aeb54b7a00781a58de7d3301c8
fearlesscardinal <140766941+fearlesscardinal@users.noreply.github.com>
2026-08-06 23:01:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 // Load settings
 let settings = {
   ip: "dirtachieved.seedloaf.gg",
-  port: 51765,
+  port: 19132,
   username: "t03cooper@gmail.com",
   offline: false,
   autoReconnect: true,
